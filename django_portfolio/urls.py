5a41e873cfe7992bd5a202a77d83125df61e8b28
SnayderCJ <Jcedenoj5@unemi.edu.ec>
2024-10-05 23:40:25 -0500

    Logeo y Registro

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -16,15 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf.urls.static import static
 from django.conf import settings
-
 from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',  views.home),
+    path('',  views.home, name='home'),
+
+    # Auth
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
+
+
+
     path('blog/', include('blog.urls'))
 ]
 
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
 from .models import Project
 
 
 def home(request):
     projects = Project.objects.all()
     return render(request, "home.html", {"projects": projects})
+
+
+# Registro:
+def signup(request):
+    if request.method == 'GET':
+        return render(request, "signup.html", {
+            'form': UserCreationForm
+        })
+    else: 
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(request.POST["username"], password=request.POST["password1"])
+                user.save()
+                login(request, user)
+                return redirect("home")
+            except IntegrityError:
+                return render(request, "signup.html", {
+                    'form': UserCreationForm,
+                    "error": "El nombre de usuario ya ha sido tomado. Por favor, elige otro nombre."
+                    })
+        else:
+            return render(request, "signup.html", {
+                'form': UserCreationForm,
+                "error": "Las contraseñas no coinciden."
+                })
+        
+
+# Cerrar sesión:
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+# Iniciar sesión:
+def signin(request):
+    if request.method == 'GET':
+        return render(request, "signin.html", {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST["username"], password=request.POST["password"])
+        if user is None:
+            return render(request, "signin.html", {
+                'form': AuthenticationForm,
+                "error": "Nombre de usuario y/o contraseña incorrectos."
+            })
+        else:
+            login(request, user)
+            return redirect("home")
